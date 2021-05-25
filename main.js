dd385ecae0f5d90028edfca7ba771f19056c35cf
Jordan Fuller <jrdnstoltz+mountainsine@gmail.com>
2021-05-25 17:05:32 -0500

    more changes to make this refactored

--- main.js
+++ main.js
@@ -1,21 +1,105 @@
-
-
 var toDos = [];
-/// TO DO 20210525 Ready From array from Local Storage  
+var retrievedData = [];
+
 var retrievedData = localStorage.getItem("testKey");
-//console.log(retrievedData);/// TEST LOG IGNORE
-/// TO DO 20210525 Deserialize string
-var toDos2 = JSON.parse(retrievedData);
-console.log(retrievedData);/// TEST LOG IGNORE
+  console.log(retrievedData);
+  // handle load event
+
+window.onload = loopMachineWacky(retrievedData)
+  if (retrievedData === '') {
+    //console.log('YAM',"");
+    document.getElementById("nothingMessageID").classList.add("hidden");
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+
+  } else {
+    //console.log(retrievedData["task"]);
+    document.getElementById("nothingMessageID").classList.remove("hidden");
+    var li = document.createElement("li");
+    var toDoValue = document.getElementById("retrievedData");
+  };
+
+
+
+
+
+
+
+
 /// TO DO 20210525 Create a function or way to loop through the tDos and call to dom ^
 
-
+/// The loop Machine Wacky attempts to do the above
 function loopMachineWacky(){
-  let thingsIHAVETODO = JSON.parse(localStorage.getItem('task'));
-  console.log(thingsIHAVETODO);
-  var loopMachineWacky = [];
+  if (retrievedData === '') {
+    //console.log('YAM');
+    document.getElementById("nothingMessageID").classList.add("hidden");
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+
+  } else {
+    // console.log('potato');
+    document.getElementById("nothingMessageID").classList.remove("hidden");
+  };
 }
-document.getElementById("myNewDIV").innerHTML = "Bonjour";
+loopMachineWacky();
+
+
+
+function renderLocalStorage(){
+    var li = document.createElement("li");
+    var toDoValue = document.getElementById("retrievedData").value;
+    var t = document.createTextNode.
+    li.appendChild(t);
+    if (retrievedData === '') {
+      // console.log('YAM');
+    } else {
+      document.getElementById("myUL").appendChild(li);
+      let myObj = {
+        task: document.getElementById("myInput").value,
+        prioritylevel: 999
+      }
+      toDos.push(myObj);
+    localStorage.setItem("testKey", JSON.stringify(toDos));
+    }
+    document.getElementById("myInput").value = "";
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+  
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+
+      }
+    };
+
+}
+
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert("You must write something!");
+    } else {
+      document.getElementById("myUL").appendChild(li);
+      let myObj = {
+        task: document.getElementById("myInput").value,
+        prioritylevel: 999
+      }
+      toDos.push(myObj);
+    }
+  }
+
+
 
  
 /// A function or way to loop through to dos and call to dom ^ 
@@ -40,7 +124,8 @@ document.getElementById("myNewDIV").innerHTML = "Bonjour";
       div.style.display = "none";
     }
   }
-  showOrHidePlaceholdMessage();
+  // showOrHidePlaceholdMessage();
+
   // Add a "checked" symbol when clicking on a list item
   var list = document.querySelector('#myUL');
   list.addEventListener('click', function(ev) {
@@ -50,7 +135,7 @@ document.getElementById("myNewDIV").innerHTML = "Bonjour";
   }, false);
   
   
-  // Create a new list item when clicking on the "Add" button
+//   // Create a new list item when clicking on the "Add" button
   function newElement() {
     var li = document.createElement("li");
     var inputValue = document.getElementById("myInput").value;
@@ -65,9 +150,8 @@ document.getElementById("myNewDIV").innerHTML = "Bonjour";
         prioritylevel: 999
       }
       toDos.push(myObj);
-      //let myObjSerialized = JSON.myObjSerialized;
-//     localStorage.setItem("testKey", JSON.stringify(myObj));// OLD local storage set for reference
-    localStorage.setItem("testKey", JSON.stringify(toDos));
+      let myObjSerialized = JSON.myObjSerialized;
+      localStorage.setItem("testKey", JSON.stringify(toDos));
     }
     document.getElementById("myInput").value = "";
 
@@ -80,26 +164,14 @@ document.getElementById("myNewDIV").innerHTML = "Bonjour";
     for (i = 0; i < close.length; i++) {
       close[i].onclick = function() {
         var div = this.parentElement;
-        div.style.display = "none";
+
       }
     }
-    showOrHidePlaceholdMessage();
-  } 
-
-
-function addListItemToDom(item){
-  
-} 
-
-
-
-function showOrHidePlaceholdMessage(){
-  var anythingIneedToDo = document.getElementsByClassName("close")
-  var i;
-  if(myNodelist.length > 0){
-    document.getElementById("nothingMessage").classList.add("hidden");
-  } else {
-    document.getElementById("nothingMessage").classList.remove("hidden");
   }
-}
+
+
+
+
+
+
 

340049cd190dc6afc46797c78f4d33b8426e895b
Jordan Fuller <jrdnstoltz+mountainsine@gmail.com>
2021-06-03 15:26:59 -0500

    another update 20210603

--- source/refactoringideas/fresh_start.js
+++ source/refactoringideas/fresh_start.js
@@ -1,12 +1,13 @@
 ///fresh_start
-let tasks = '1';
+let tasks = '';
 console.log(tasks);
 var tag_id = document.getElementById('myRenderDiv');
 
 function doYouGotAnyTaskstoDo(tasks) {
-    if (tasks) {
+    if (tasks && tasks.length > 0 ) {
         tag_id.innerHTML = 'This is a literal string but generated from JS into the innerHTML via tag ID';
     } else {
+        tag_id.innerHTML = '';
         console.log('you got nothing to do');
     }
     

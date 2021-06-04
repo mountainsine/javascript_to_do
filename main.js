20626a223e07e1ff86c7a532f11c62056da72805
Jordan Fuller <jrdnstoltz+mountainsine@gmail.com>
2021-06-04 11:19:43 -0500

    changes 20210604

--- main.js
+++ main.js
@@ -2,23 +2,25 @@
 var toDos = [];
 var retrievedDataArray = [];
 var tag_id = document.getElementById('myRenderDiv3');
+var the = document.getElementById('myUL');
 var myObj = [];
 //var parsedArrayObject = [JSON.parse(window.localStorage.getItem('testKey'))];
 var myNodelist = [];
 //var myNodelist = document.getElementsByTagName("li");
 
 // RETRIEVES LOCAL STORAGE ITEMS
-var retrievedDataArray = JSON.parse(localStorage.getItem("testKey"));
+
 
 function validateToDoFunction(){
+  var retrievedDataArray = JSON.parse(localStorage.getItem("testKey"));
   if (retrievedDataArray && retrievedDataArray.length > 0){
-    //console.log("LOCALSTORAGEDETECTED!")
+    console.log("LOCALSTORAGEDETECTED!")
     console.log('Stored Item Length = ' + retrievedDataArray.length);
 
     renderLocalStorage2();
   } else {
-    //tag_id.innerHTML = '&#x2705 You have no Tasks!';
-    //console.log("NO LOCAL STORAGE FOUND!");
+    tag_id.innerHTML = '&#x2705 You have no Tasks!';
+    console.log("NO LOCAL STORAGE FOUND!");
   };
 };
 
@@ -29,44 +31,48 @@ window.onload = function() {
 
 
 function renderLocalStorage2() {
-  if (retrievedDataArray && retrievedDataArray.length > 0){
-    var currentToDoString = JSON.stringify(retrievedDataArray[0]);
-    tag_id.innerHTML = currentToDoString;
-  } else {
-    console.log('renderLocalStorage2 FAILURE!');
-    tag_id.innerHTML = '&#x2705 You have no Tasks!';
-    
+    if (retrievedDataArray && retrievedDataArray.length > 0){
+      console.log('We Are invoking the "renderLocalStorage2" function now');
+      var seperatedToDoItem = JSON.stringify(retrievedDataArray[0]);
+      console.log(seperatedToDoItem);
+      var dataExampleVariableForTesting = JSON.parse(localStorage.getItem("testKey"));
+      tag_id.innerHTML = dataExampleVariableForTesting;
+    } else {
+      console.log('renderLocalStorage2 FAILURE!');
+      tag_id.innerHTML = '&#x2705 You have no Tasks!';
+      
+    };
   };
-};
+
 
 
 
 
 
 // ANOTHER attempt at loop through the tDos and call to dom 
-function renderLocalStorage(){
-  if (retrievedDataArray && retrievedDataArray.length > 0){
-    //document.getElementById("myUL").appendChild("test");
-    var t = document.createElement("li");
-    var li = document.createElement("li");
-   // let myObj = JSON.stringify(retrievedDataArray[0]);
-    toDos.push(myObj);
-    document.getElementById("myUL").appendChild(li);
-    let myObjSerialized = JSON.myObjSerialized;
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "localstorage";
-    for (i = 0; i < close.length; i++) {
-      close[i].onclick = function() {
-        var div = this.parentElement;
-        window.onload
+// function renderLocalStorage(){
+//   if (retrievedDataArray && retrievedDataArray.length > 0){
+//     //document.getElementById("myUL").appendChild("test");
+//     // var t = document.createElement("li");
+//     // var li = document.createElement("li");
+//    // let myObj = JSON.stringify(retrievedDataArray[0]);
+//     // toDos.push(myObj);
+//     // document.getElementById("myUL").appendChild(li);
+//     // let myObjSerialized = JSON.myObjSerialized;
+//     // var span = document.createElement("SPAN");
+//     // var txt = document.createTextNode("\u00D7");
+//     // span.className = "localstorage";
+//     // for (i = 0; i < close.length; i++) {
+//     //   close[i].onclick = function() {
+//     //     var div = this.parentElement;
+//     //     window.onload
 
-      }
-    }
-  } else {
+//       }
+//     }
+//   } else {
     
-  }
-}
+//   }
+// }
   
 // Create a "close" button and append it to each list item
   var myNodelist = document.getElementsByTagName("LI");
@@ -100,7 +106,7 @@ function renderLocalStorage(){
   
   ///Create a new list item when clicking on the "Add" button
   function newElement() {
-    console.log('YOYOYOYO');
+    console.log('We Are invoking the "newElement" function now');
     var li = document.createElement("li");
     var inputValue = document.getElementById("myInput").value;
     var t = document.createTextNode(inputValue);

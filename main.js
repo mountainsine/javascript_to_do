d246cc7e857e59da5f4c58ead09bf102313e37f4
Jordan Fuller <jrdnstoltz+mountainsine@gmail.com>
2021-06-08 01:01:09 -0500

    updates 20210608

--- main.js
+++ main.js
@@ -35,15 +35,16 @@ function hideDefaultMessage(){
 }
 
 // STEP 4 - Create a "close" button and append it to each list item
-  var myNodelist = document.getElementsByTagName("LI");
-  var i;
-  for (i = 0; i < myNodelist.length; i++) {
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    myNodelist[i].appendChild(span);
-  }
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
 
   // STEP 5 - Click on a close button to hide the current list item
   var close = document.getElementsByClassName("close");
@@ -120,6 +121,7 @@ function renderThatLocalStorageData(){
   for (var i = 0; i < arrayLength; i++) {
     var li = document.createElement("li");
     var t = document.createTextNode(myStringArray[i].task);
+    li.classList.add("centeredListText");
     li.appendChild(t);
       document.body.appendChild(li);
       document.createElement("li");

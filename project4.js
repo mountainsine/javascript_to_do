2b0e21a3ae1232c02f98ee212f7f7880598b0fd9
Jordan Fuller <jrdnstoltz+mountainsine@gmail.com>
2021-06-16 15:24:49 -0500

    Updates
    Added image directory to git ignore. tuned projects 2-3

---
+++ project4.js
@@ -0,0 +1,11 @@
+const KAIJU = [
+    { name: 'Godzilla', tier: 1 },
+    { name: 'King Kong', tier: 2 },
+    { name: 'Gamera', tier: 1 },
+    { name: 'Mothra', tier: 4 },
+    { name: 'Ghidora', tier: 4 },
+    { name: 'Ultraman', tier: 1 },
+    { name: 'Rodan', tier: 4 },
+    { name: 'Minya', tier: 4 },
+    { name: 'Son Of Kong', tier: 4 },
+    ]
